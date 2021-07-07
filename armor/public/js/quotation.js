5c5cb4eb9d97dd93fdcb5d1099894ebfbb637fe4
ashish-greycube <mr.ashish.shah@gmail.com>
2021-07-07 10:23:00 +0530

    fix: max_discount from customer_group direct, not via customer

--- armor/public/js/quotation.js
+++ armor/public/js/quotation.js
@@ -2,46 +2,31 @@
 
 frappe.ui.form.on(cur_frm.doctype, {
   onload_post_render: function (frm) {
-    debugger
-    if (frm.doc.quotation_to=='Lead') {
+    if (frm.doc.quotation_to == 'Lead') {
       frappe.db.get_value('Lead', frm.doc.party_name, 'guest_group_cf')
-    .then(r => {
-      let customer_group=r.message.guest_group_cf
-      frm.set_value('customer_group', customer_group)
-    })    
-    }
-    if (frm.doc.customer) {
-      frappe.db.get_value('Customer', frm.doc.customer, 'customer_group')
-      .then(r => {
-        let customer_group = r.message.customer_group
-        if (customer_group) {
-          frappe.db.get_value('Customer Group', customer_group, 'max_discount_allowed_cf')
-            .then(r => {
-              let max_discount_allowed_cf = r.message.max_discount_allowed_cf
-              if (max_discount_allowed_cf) {
-                frm.set_value('max_discount_allowed_cf', max_discount_allowed_cf)
-              }
-            })
-        }
-      })      
+        .then(r => {
+          let customer_group = r.message.guest_group_cf
+          frm.set_value('customer_group', customer_group)
+        })
     }
   },
-  party_name:function (frm) {
-    if (frm.doc.quotation_to=='Lead') {
+  party_name: function (frm) {
+    if (frm.doc.quotation_to == 'Lead') {
       frappe.db.get_value('Lead', frm.doc.party_name, 'guest_group_cf')
-    .then(r => {
-      let customer_group=r.message.guest_group_cf
-      frm.set_value('customer_group', customer_group)
-    })    
+        .then(r => {
+          let customer_group = r.message.guest_group_cf
+          frm.set_value('customer_group', customer_group)
+        })
     }
   },
-  customer_group:function (frm) {
+  customer_group: function (frm) {
     if (frm.doc.customer_group) {
       frappe.db.get_doc('Customer Group', frm.doc.customer_group)
-      .then(doc => {
-          let customer_group_source=doc.customer_group_source
+        .then(doc => {
+          let customer_group_source = doc.customer_group_source
           let sales_partners = []
-          if (customer_group_source=='Is Agency') {
+          let max_discount_allowed_cf = doc.max_discount_allowed_cf
+          if (customer_group_source == 'Is Agency') {
             frm.set_df_property('source', 'reqd', 1)
             frm.set_df_property('sales_partner', 'reqd', 1)
             $.each(doc.sales_partners_cf, function (index, source_row) {
@@ -54,8 +39,8 @@ frappe.ui.form.on(cur_frm.doctype, {
                 }
               }
             })
-          
-          }else if(customer_group_source=='Walk In'){
+
+          } else if (customer_group_source == 'Walk In') {
             frm.set_df_property('source', 'reqd', 1)
             frm.set_df_property('sales_partner', 'reqd', 1)
             $.each(doc.sales_partners_cf, function (index, source_row) {
@@ -67,41 +52,36 @@ frappe.ui.form.on(cur_frm.doctype, {
                   partner_name: ['in', sales_partners]
                 }
               }
-            })            
-          }else if(customer_group_source=='Social Media'){
+            })
+          } else if (customer_group_source == 'Social Media') {
             frm.set_df_property('source', 'reqd', 1)
             frm.set_df_property('sales_partner', 'reqd', 0)
-          }else{
+          } else {
             frm.set_df_property('source', 'reqd', 0)
             frm.set_df_property('sales_partner', 'reqd', 0)
           }
-          frm.refresh_field('sales_partner')      
-          frm.refresh_field('source')      
-      })          
-      
+
+          frm.set_value('max_discount_allowed_cf', max_discount_allowed_cf)
+
+          frm.refresh_field('max_discount_allowed_cf')
+          frm.refresh_field('sales_partner')
+          frm.refresh_field('source')
+        })
+
     }
   },
-  validate:function (frm) {
-    if (frm.doc.quotation_to==undefined || frm.doc.quotation_to!='Lead') {
-      frappe.throw({message:__("Please select 'Quotation To' field value as Lead."),title:__("Quotation To error." )})
+  validate: function (frm) {
+    if (frm.doc.quotation_to == undefined || frm.doc.quotation_to != 'Lead') {
+      frappe.throw({
+        message: __("Please select 'Quotation To' field value as Lead."),
+        title: __("Quotation To error.")
+      })
     }
-    if (frm.doc.party_name==undefined || frm.doc.party_name =='') {
-      frappe.throw({message:__("Please select lead."),title:__("Lead value is missing." )})
-    }    
-  },
-  customer_name: function (frm) {
-    frappe.db.get_value('Customer', frm.doc.party_name, 'customer_group')
-    .then(r => {
-        let customer_group=r.message.customer_group
-        if (customer_group) {
-          frappe.db.get_doc('Customer Group', customer_group)
-          .then(doc => {
-              let max_discount_allowed_cf=doc.max_discount_allowed_cf
-              if (max_discount_allowed_cf) {
-              frm.set_value('max_discount_allowed_cf', max_discount_allowed_cf)
-              }
-          })        
-        }
-    })
+    if (frm.doc.party_name == undefined || frm.doc.party_name == '') {
+      frappe.throw({
+        message: __("Please select lead."),
+        title: __("Lead value is missing.")
+      })
+    }
   }
 })
