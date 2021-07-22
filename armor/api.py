6de2f5fb30d04b7ba7d7a93953b46914a55eebad
ashish-greycube <mr.ashish.shah@gmail.com>
2021-07-22 09:30:10 +0530

    fix: jignesh testing

--- armor/api.py
+++ armor/api.py
@@ -165,7 +165,7 @@ def _make_customer(source_name, target_doc=None, ignore_permissions=False):
 		else:
 			target.customer_type = "Individual"
 			target.customer_name = source.lead_name
-		target.phone_number_cf=source.phone
+		# target.phone_number_cf=source.phone
 		target.customer_group = source.guest_group_cf or frappe.db.get_default("Customer Group")
 
 	doclist = get_mapped_doc("Lead", source_name,
